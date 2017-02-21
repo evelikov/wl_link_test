9ce2a55e3cd8b20f04374d2583c5c1dcb118abd4
Emil Velikov <emil.l.velikov@gmail.com>
2017-02-21 15:34:33 +0000

    Import dummy library
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <wayland-util.h>
+#include <wayland-client.h>
+#include <wayland-server.h>
+
+
+int
+main(void)
+{
+    struct wl_list list;
+
+    wl_list_init(&list);
+
+    struct wl_array array;
+
+    memset(&array, 0x57, sizeof array);
+
+    wl_array_init(&array);
+    return 0;
+}
+
